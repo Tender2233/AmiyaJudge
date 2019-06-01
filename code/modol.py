01e79cddee4590cd666a3797fcfc1543bce3626b
Tender2233 <tengda2233@126.com>
2019-06-01 19:54:07 +0800

    增加卷积池化层，添加dropout层

--- code/modol.py
+++ code/modol.py
@@ -5,6 +5,7 @@ import tensorflow as tf
 import time
 import os
 from getData import *
+os.environ["CUDA_VISIBLE_DEVICES"] = "3"
 #hyper things
 train_epochs = 10
 batch_size = 10
@@ -12,7 +13,7 @@ learning_rate = 0.01
 test_ratio = 0.1
 
 #the picture size
-imginput = Input(shape=(100, 100, 3))
+imginput = Input(shape=(100, 100, 1))
 
 #imgtube1
 imgtube1 = Conv2D(filters=32, kernel_size=3, padding='same',
@@ -38,6 +39,7 @@ imgtube2 = AveragePooling2D(padding="valid",
                             pool_size=2,
                             data_format="channels_last",
                             strides=1)(imgtube2)
+imgtube2=Dropout(0.2)(imgtube2)
 imgtube2 = Flatten()(imgtube2)
 
 #imgtube3
@@ -60,10 +62,41 @@ imgtube3 = AveragePooling2D(padding="valid",
                             pool_size=2,
                             data_format="channels_last",
                             strides=1)(imgtube3)
+imgtube3 = BatchNormalization()(imgtube3)
+imgtube3 = Conv2D(filters=32, kernel_size=3, padding='same',
+                  activation='relu')(imgtube3)
+imgtube3 = BatchNormalization()(imgtube3)
+imgtube3 = Conv2D(filters=32, kernel_size=3, padding='same',
+                  activation='relu')(imgtube3)
+imgtube3 = BatchNormalization()(imgtube3)
+imgtube3 = Conv2D(filters=32, kernel_size=1, padding='same',
+                  activation='relu')(imgtube3)
+imgtube3 = BatchNormalization()(imgtube3)
+imgtube3 = AveragePooling2D(padding="valid",
+                            pool_size=2,
+                            data_format="channels_last",
+                            strides=1)(imgtube3)
+imgtube3 = BatchNormalization()(imgtube3)
+imgtube3 = Conv2D(filters=32, kernel_size=3, padding='same',
+                  activation='relu')(imgtube3)
+imgtube3 = BatchNormalization()(imgtube3)
+imgtube3 = Conv2D(filters=32, kernel_size=3, padding='same',
+                  activation='relu')(imgtube3)
+imgtube3 = BatchNormalization()(imgtube3)
+imgtube3 = Conv2D(filters=32, kernel_size=1, padding='same',
+                  activation='relu')(imgtube3)
+imgtube3 = BatchNormalization()(imgtube3)
+imgtube3 = AveragePooling2D(padding="valid",
+                            pool_size=2,
+                            data_format="channels_last",
+                            strides=1)(imgtube3)
+imgtube3 = Dropout(0.2)(imgtube3)                    
 imgtube3 = Flatten()(imgtube3)
 
 #combine network
 comnet = concatenate([imgtube1, imgtube2, imgtube3])
+comnet = Dense(256, activation='relu')(comnet)
+comnet = Dense(128, activation='relu')(comnet)
 comnet = Dense(64, activation='relu')(comnet)
 output = Dense(3, activation='softmax')(comnet)
 
@@ -79,5 +112,7 @@ model.compile(optimizer=Adam(learning_rate),
 
 
 x_data,y_data=fetData("tfrecord_train.tfrecords")
+# print(x_data.shape)
+# print(y_data.shape)
 # print(x_data,y_data)
-model.fit(x_data,y_data,validation_split=0.15, epochs=20, batch_size=30)
+model.fit(np.array(x_data),np.array(y_data),validation_split=0.3, epochs=30, batch_size=100,shuffle=True)
